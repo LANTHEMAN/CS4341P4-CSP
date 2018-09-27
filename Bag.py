297d7c523693dce34f6c4871cf84501d23d6f476
alaza <alazar.genene@gmail.com>
2018-09-27 07:59:38 -0400

    Fixed some bugs of the Bag class after checking it out

--- Bag.py
+++ Bag.py
@@ -6,18 +6,19 @@
 # Function to put items in the bag
 # Function to remove items from the bag
 
-class Bag:
+
+class Bag():
 
     # The class constructor for the bag class containing the capacity, current_load, contains, and the name attributes
     def __init__(self, capacity=0, current_load=0, contains=0, name=""):
-        self.capacity = capacity
-        self.current_load = current_load
-        self.contains = []
-        self.name = name
+        self.capacity = capacity if capacity is None else 0
+        self.current_load = current_load if current_load is None else 0
+        self.contains = contains if contains is None else []
+        self.name = name if name is None else 0
 
     # Checks if the current capacity isn't met
     def can_add_item(self):
-        if self.current_load <= 1:
+        if self.current_load <= 1 and self.capacity != 0:
             return True
         else:
             return False
@@ -28,12 +29,14 @@ class Bag:
 
     # Adds an item to a bag
     def add_item(self, item):
-        if self.can_add_item():
+        if self.can_add_item(self):
             self.contains.append(item)
-            self.calculate_current()
+            self.current_load = self.calculate_current(self)
         else:
             print("The threshold for this Bag has been met, you cannot add anymore items.")
             # For now we'll print out that we cannot handle anymore items
+
+
 
 
 
---
+++ CSP.py
@@ -0,0 +1,7 @@
+import Bag
+
+b = Bag.Bag
+b.__init__(b)
+b.capacity=1
+b.add_item(b, "B")
+print()
